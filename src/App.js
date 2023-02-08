e7e2d911f3a43d962cc6365311fe65c9a0281c2e
guntsadze <dimitri.guncadze@gmail.com>
2023-02-08 22:37:06 +0400

    create accordion with functionally but not working animation yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AccordionItem from "./components/AccordionItem";
+
+const img = {
+  woman: "assets/illustration-woman-online-desktop.svg",
+  box: "assets/illustration-box-desktop.svg",
+};
 
 function App() {
+  const [activeAccordionNumber, setActiveAccordionNumber] = useState(2);
+
+  const DATA = [
+    {
+      itemNumber: 1,
+      title: "How many team members can I invite?",
+      content: "content 1",
+    },
+    {
+      itemNumber: 2,
+      title: "What is the maximum file upload size?",
+      content:
+        "No more than 2GB. All files in your account must fit your allotted storage space.",
+    },
+    {
+      itemNumber: 3,
+      title: "How do I reset my password?",
+      content: "content 3",
+    },
+    {
+      itemNumber: 4,
+      title: "Can I cancel my subscription?",
+      content: "content 4",
+    },
+    {
+      itemNumber: 5,
+      title: "Do you provide additional support?",
+      content: "content 5",
+    },
+  ];
+
+  const handleClick = (accordionNumber) => {
+    if (activeAccordionNumber === accordionNumber) {
+      setActiveAccordionNumber(null);
+    } else {
+      setActiveAccordionNumber(accordionNumber);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="accordion-container">
+        <div className="accordion-div">
+          <div className="woman-div">
+            <img className="woman" src={img.woman} />
+            <img className="box" src={img.box} />
+          </div>
+          <div className="accordion-card">
+            <h1> FAQ</h1>
+            {DATA.map((data) => (
+              <AccordionItem
+                activeAccordionNumber={activeAccordionNumber}
+                handleClick={handleClick}
+                itemNumber={data.itemNumber}
+                title={data.title}
+                content={data.content}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/AccordionItem.js
@@ -0,0 +1,11 @@
+export default function AccordionItem(props) {
+  const { activeAccordionNumber, handleClick, itemNumber, title, content } =
+    props;
+
+  return (
+    <div className="content-div" onClick={() => handleClick(itemNumber)}>
+      {title}
+      {activeAccordionNumber === itemNumber ? <p>{content}</p> : null}
+    </div>
+  );
+}
